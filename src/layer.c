d5bd50e101e8f0669a08de0046ab461dc1962791
Gustavo6046 <rehermann6046@gmail.com>
2021-04-17 01:38:20 -0300

    Refactor layer weight allocation
    It used to reside in the init callback.
    
    This also adds a zero_weights function to zero a layer's weights.

--- src/layer.c
+++ src/layer.c
@@ -52,7 +52,7 @@ static void layer_proc_linear(struct l_layer *layer, float *inputs, float *outpu
 
 static void layer_init_linear(struct l_layer *layer) {
     layer->state = 0;
-    layer->weights = malloc(sizeof(float) * layer->out_size * (/*biases*/ 1 + /*weights*/ layer->in_size));
+    layer->num_weights = layer->out_size * (/*biases*/ 1 + /*weights*/ layer->in_size);
 }
 
 // quadratic
@@ -86,7 +86,7 @@ static void layer_proc_quadratic(struct l_layer *layer, float *inputs, float *ou
 
 static void layer_init_quadratic(struct l_layer *layer) {
     layer->state = 0;
-    layer->weights = malloc(sizeof(float) * layer->out_size * (/*biases*/ 1 + /*weights*/ layer->in_size * 2));
+    layer->num_weights = layer->out_size * (/*biases*/ 1 + /*weights*/ layer->in_size * 2);
 }
 
 ///////
@@ -96,10 +96,19 @@ void l_layer_init(struct l_layer *layer, struct l_layer_type *type, f_activation
     layer->out_size = out_size;
     layer->activation = activation;
     layer->type = type;
+    layer->input_buffer = malloc(sizeof(float) * in_size);
 
     type->init(layer);
 
-    layer->input_buffer = malloc(sizeof(float) * in_size);
+    if (layer->num_weights > 0) {
+        layer->weights = malloc(sizeof(float) * layer->num_weights);
+    }
+}
+
+void l_layer_zero_weights(struct l_layer *layer) {
+    for (int wi = 0; wi < layer->num_weights; wi++) {
+        layer->weights[wi] = 0.0f;
+    }
 }
 
 void l_layer_deinit(struct l_layer *layer) {
