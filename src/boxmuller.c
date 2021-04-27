9eb42a741e3b1521854bfd9b29ef84193ff8d00b
Gustavo6046 <rehermann6046@gmail.com>
2021-04-26 23:11:01 -0300

    Refactor and tweak code in boxmuller.c

--- src/boxmuller.c
+++ src/boxmuller.c
@@ -6,14 +6,16 @@
 #include "rng.h"
 
 
-static void bm_update(struct bm_state *state) {
+float bm_next(struct bm_state *state) {
     const float mag = state->sigma * sqrtf(2.0 * logf(state->a));
 
     const float z1 = mag * cosf(two_pi * state->b) + state->mu;
     const float z2 = mag * sinf(two_pi * state->b) + state->mu;
 
-    state->a = state->b;
-    state->b = z1;
+    state->a = state->b * z1;
+    state->b = rng_random_float(1.0);
+
+    return z2;
 }
 
 void bm_state_init(struct bm_state *state, float mu, float sigma) {
@@ -25,12 +27,4 @@ void bm_state_init(struct bm_state *state, float mu, float sigma) {
 
     state->mu = mu;
     state->sigma = sigma;
-
-    bm_update(state);
-}
-
-float bm_next(struct bm_state *state) {
-    bm_update(state);
-
-    return state->b;
 }
