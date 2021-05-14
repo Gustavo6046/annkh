4a5a2627f5de8750ecf9bd1047c43479c0ef9a53
Gustavo6046 <rehermann6046@gmail.com>
2021-05-14 16:28:21 -0300

    Add MAIN_CALL for using error.h from main

--- src/errorc.h
+++ src/errorc.h
@@ -2,6 +2,7 @@
 
 #define ERR_RET(errsig) { return -errsig; }
 #define CALL_RET(val) { int err = (val); if (!es_code_is_success(err)) { return err; } }
+#define MAIN_CALL(val, callback, sig) { int err = (val); if (!es_code_is_success(err)) { (callback); return (sig); } }
 #define SUCCESS { return ES_SUCCESS; }
 #define EMPTY_SUCCESS { return ES_SUCCESS_NO_VALUE; }
 
